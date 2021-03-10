e0cc3389344ab6b4dd810bf9b1b543483c29742c
Josh Fish <jwee761@gmail.com>
2021-03-10 14:44:12 -0800

    get stored book list route

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,12 @@
-module.exports = function (app) {
+const db = require("../models");
 
+module.exports = function (app) {
+    app.get("/api/books", function (req, res) {
+        db.Book.find()
+            .then(books => res.status(200).json(books))
+            .catch(err => {
+                console.error(err);
+                res.status(500).end();
+            });
+    });
 };
