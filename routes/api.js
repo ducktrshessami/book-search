d1e2db92dc80eb2476663ac8e3dcb25cd28753f9
Josh Fish <jwee761@gmail.com>
2021-03-10 14:59:25 -0800

    delete by id route

--- routes/api.js
+++ routes/api.js
@@ -18,4 +18,13 @@ module.exports = function (app) {
                 res.status(500).end();
             });
     });
+
+    app.delete("/api/books/:id", function (req, res) {
+        db.Book.findByIdAndDelete(req.params.id)
+            .then(() => res.status(200).end())
+            .catch(err => {
+                console.error(err);
+                res.status(400).end();
+            });
+    });
 };
