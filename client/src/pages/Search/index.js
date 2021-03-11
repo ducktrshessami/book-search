7e3c6775057c74b1abcebbcf7c854e3c8105a7ef
Josh Fish <jwee761@gmail.com>
2021-03-11 15:04:49 -0800

    search page

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import BookSearch from "../../components/BookSearch";
+import BookList from "../../components/BookList";
+import bookSearch from "../../utils/bookSearch";
+
 export default function Search() {
+    const [searchResults, setSearch] = useState([]);
+
+    function search(query) {
+        bookSearch(query)
+            .catch(console.error);
+    }
+
     return (
-        <div></div>
+        <section>
+            <BookSearch onSubmit={search} />
+            <BookList title="Results" books={searchResults} saved={false} />
+        </section>
     );
 };
