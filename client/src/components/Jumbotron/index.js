f0ee90603070a102fa4fce6d5067695c553a14d2
Josh Fish <jwee761@gmail.com>
2021-03-11 13:52:06 -0800

    title card jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,5 +1,10 @@
+import "./Jumbotron.css";
+
 export default function Jumbotron() {
     return (
-        <div></div>
+        <article className="title-card text-center m-5 p-5">
+            <h1>(React) Google Books Search</h1>
+            <h3>Search for and Save Books of Interest</h3>
+        </article>
     );
 };
