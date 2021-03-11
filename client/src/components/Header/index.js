1ef1ee09a92911b5296977c62dc568c98be8bdf4
Josh Fish <jwee761@gmail.com>
2021-03-11 11:46:22 -0800

    placeholder components

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import Navbar from "../Navbar";
+import Jumbotron from "../Jumbotron";
+
+export default function Header() {
+    return (
+        <header>
+            <Navbar />
+            <Jumbotron />
+        </header>
+    );
+};
