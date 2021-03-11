46fd8830a00eeeaa15681149fae2cd976cc3c437
Josh Fish <jwee761@gmail.com>
2021-03-11 14:37:02 -0800

    saved book list rendering

--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,5 +1,19 @@
-export default function Saved() {
-    return (
-        <div></div>
-    );
+import { Component } from "react";
+import BookList from "../../components/BookList";
+import { get } from "../../utils/api";
+
+export default class Saved extends Component {
+    state = { books: [] }
+
+    componentDidMount() {
+        get()
+            .then(books => this.setState({ books: books }))
+            .catch(console.error);
+    }
+
+    render() {
+        return (
+            <BookList title="Saved Books" books={this.state.books} />
+        );
+    }
 };
