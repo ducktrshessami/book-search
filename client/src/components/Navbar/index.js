6b128eb6882848b2cc2f40179fa30fc32948a7b2
Josh Fish <jwee761@gmail.com>
2021-03-11 12:41:58 -0800

    navbar links

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,5 +1,27 @@
+import {
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import logo from "./logo.png";
+import "./Navbar.css";
+
 export default function Navbar() {
     return (
-        <nav></nav>
+        <nav className="row">
+            <Link to="/" className="logo">
+                <img src={logo} alt="Google Books" />
+            </Link>
+            <Switch>
+                <Route path="/saved">
+                    <Link to="/">Search</Link>
+                    <div className="active">Saved</div>
+                </Route>
+                <Route path="/">
+                    <div className="active">Search</div>
+                    <Link to="/saved">Saved</Link>
+                </Route>
+            </Switch>
+        </nav>
     );
 };
