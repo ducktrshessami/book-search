93b8e9f6ec248749b02009ddd16a776e83a2fda9
ducktrshessami <jwee761@gmail.com>
2021-03-11 22:39:12 -0800

    converted Book to component

--- client/src/components/BookList/Book.js
+++ client/src/components/BookList/Book.js
@@ -1,34 +1,43 @@
+import { Component } from "react";
 import API from "../../utils/api";
 
-export default function Book(props) {
-    function save() {
-        API.post(props);
+export default class Book extends Component {
+    state = { id: this.props._id }
+
+    save() {
+        API.post(this.props)
+            .then(data => this.setState({ id: data._id }))
+            .catch(console.error);
     }
 
-    function remove() {
-        API.delete(props._id);
+    remove() {
+        API.delete(this.props._id)
+            .then(() => this.setState({ id: "" }))
+            .catch(console.error);
     }
 
-    function button(saved) {
-        if (saved) {
-            return <button onClick={remove} className="btn btn-danger mx-1">Delete</button>;
+    button() {
+        if (this.state.id) {
+            return <button onClick={() => this.remove()} className="btn btn-danger mx-1">Delete</button>;
         }
         else {
-            return <button onClick={save} className="btn btn-success mx-1">Save</button>;
+            return <button onClick={() => this.save()} className="btn btn-success mx-1">Save</button>;
         }
     }
 
-    return (
-        <li className="black-border mx-1 p-5">
-            <div className="float-right">
-                <a href={props.link} className="mx-1 btn btn-secondary">View</a>
-                {button(props.saved)}
-            </div>
-            <h5>{props.title}</h5>
-            {props.authors.length ? <h6>Written By {props.authors.join(", ")}</h6> : undefined}
-            <br />
-            <img src={props.image} alt={props.title} className="float-left mt-2 mr-3" />
-            <p>{props.description}</p>
-        </li>
-    );
+    render() {
+        return (
+            <li className="black-border mx-1 p-5">
+                <div className="float-right">
+                    <a href={this.props.link} className="mx-1 btn btn-secondary">View</a>
+                    {this.button()}
+                </div>
+                <h5>{this.props.title}</h5>
+                {this.props.authors.length ? <h6>Written By {this.props.authors.join(", ")}</h6> : undefined}
+                <br />
+                <img src={this.props.image} alt={this.props.title} className="float-left mt-2 mr-3" />
+                <p>{this.props.description}</p>
+            </li>
+        );
+    }
 };
