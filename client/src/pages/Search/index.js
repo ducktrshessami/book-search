066081a33d79dee73614b18cda14c69600d074eb
Josh Fish <jwee761@gmail.com>
2021-03-11 17:05:04 -0800

    handle dynamic search page rendering

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -4,17 +4,21 @@ import BookList from "../../components/BookList";
 import bookSearch from "../../utils/bookSearch";
 
 export default function Search() {
-    const [searchResults, setSearch] = useState([]);
+    const [searchQuery, setQuery] = useState("");
+    const [searchResults, setResults] = useState([]);
 
     function search(query) {
+        setQuery(query);
         bookSearch(query)
+            .then(res => res.items)
+            .then(setResults)
             .catch(console.error);
     }
 
     return (
         <section>
             <BookSearch onSubmit={search} />
-            <BookList title="Results" books={searchResults} saved={false} />
+            {searchQuery ? <BookList title="Results" books={searchResults} saved={false} /> : undefined}
         </section>
     );
 };
